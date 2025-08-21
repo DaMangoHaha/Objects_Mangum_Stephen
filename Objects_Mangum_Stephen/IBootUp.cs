3662f9c90607606a9de8d4aa98650b6c0ee14e4c
Stephen Mangum <stephenmangum01@gmail.com>
2025-08-21 09:31:00 -0500

    Add Computer class and IBootUp interface, update Program
    Introduced a new Computer class implementing the IBootUp interface, which provides properties and methods for simulating computer power states. Updated Program.cs to demonstrate object instantiation, interface usage, string manipulation, and method calls related to the new Computer class.

---
+++ Objects_Mangum_Stephen/IBootUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_Mangum_Stephen
+{
+    internal interface IBootUp
+    {
+
+        public bool IsOn { get; set; }
+
+        public void PowerOnOff();
+    }
+}
